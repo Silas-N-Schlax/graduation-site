87a53f42ce604757232ca643293b852b4e6c801b
SilasNS <107065641+silasSN@users.noreply.github.com>
2025-03-22 22:23:52 -0400

    Added rest of passthrough's and dir roots

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,21 @@
 module.exports = function(eleventyConfig) {
   // Add Passthrough Copy for CSS & JS
-  eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addPassthroughCopy("js");
+  eleventyConfig.addPassthroughCopy("src/assets/css");
+  eleventyConfig.addPassthroughCopy("src/assets/js"); 
   eleventyConfig.addPassthroughCopy("src/_redirects");
+  eleventyConfig.addPassthroughCopy("src/assets/files");
+  eleventyConfig.addPassthroughCopy("src/assets/images"); 
+
+  eleventyConfig.addCollection("all", function(collection) {
+    return collection.getAll();
+  });
 
   return {
     dir: {
       input: "src",  // Source folder
       output: "_site", // Output folder
-      includes: "_includes" // Includes folder
+      includes: "_includes", // Includes folder
+      data: "_data" // Data folder
     }
   };
 };
